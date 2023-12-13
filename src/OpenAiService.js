2e4c8388ea71709b38ddf2d45b03a138d6e03b09
Max Bachhuber <max.bachhuber@bahuma.io>
2023-12-13 10:28:17 +0100

    Switch used model from "text-davinci-003" to "gpt-3.5-turbo-instruct" because the davinci model will be disabled on 01. January 2024

--- src/OpenAiService.js
+++ src/OpenAiService.js
@@ -3,7 +3,7 @@ import {getConfigVariable} from "./util.js";
 
 export default class OpenAiService {
     #openAi;
-    #model = "text-davinci-003";
+    #model = "gpt-3.5-turbo-instruct";
 
     constructor() {
         const apiKey = getConfigVariable("OPENAI_API_KEY")
